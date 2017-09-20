efd912078ef3ddc51f20f1812c65fdeaffc8148a
Denis Tisov <dinyat@yandex-team.ru>
2017-09-20 12:59:52 +0300

    fix

--- src/main/java/ru/java/course/homework/reznikova/elena/HomeWorkTask2.java
+++ src/main/java/ru/java/course/homework/reznikova/elena/HomeWorkTask2.java
@@ -1,17 +1,23 @@
 package ru.java.course.homework.reznikova.elena;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class HomeWorkTask2 {
     public static void main(String[] args) {
+        while (!doTask().contains(0)) {
+
+        }
+    }
+
+    private static List<Integer> doTask() {
         Random random = new Random();
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < 12; i++) {
-            list.add(random.nextInt(11));
+            list.add(random.nextInt(10) + 1);
         }
-
-        int countNegative = 0; //обертки стоит использовать только в коллекция типа ArrayList и т.д.
+        Integer countNegative = 0;
         while (countNegative < 6) {
             int i = random.nextInt(12);
             if (list.get(i) > 0) {
@@ -19,9 +25,7 @@ public class HomeWorkTask2 {
                 countNegative++;
             }
         }
-        System.out.println("Результат:");
-        for (Integer aList : list) {
-            System.out.println(aList);
-        }
+        System.out.println("Результат: " + list);
+        return list;
     }
 }
